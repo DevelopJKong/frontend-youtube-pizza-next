c5889521bc1751c12a723f79932f69c262032824
cafe small house <ybaram40@gmail.com>
2023-10-03 09:47:13 +0900

    기본 세팅 구성

---
+++ tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1280px',
+    },
+    extend: {
+      backgroundImage: {
+        pattern: "url('/pattern.png')",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        bangers: [`var(--font-bangers)`, 'sans-serif'],
+        quicksand: [`var(--font-quicksand)`, 'sans-serif'],
+        robotoCondensed: [`var(--font-robotoCondensed)`, 'sans-serif'],
+      },
+      colors: {
+        primary: '#d1411e',
+        secondary: '#ffa323',
+        tertiary: '#331812',
+        black: '#231714',
+        orange: '#FF7A30',
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+};
+export default config;
